dd8b97baeb994e23239f85455509ab25d56ea1ac
“Marcelo <marcelo.assim@gmail.com>
2023-02-23 23:39:07 -0300

    Base do projeto

---
+++ usuarios/admin.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import admin as admin_auth_django
+from django.contrib import admin
+from .models import Users
+
+from .forms import UserChangeForm,UserCreationForm
+
+
+@admin.register(Users)
+class UsersAdmin(admin_auth_django.UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = Users
+    # fieldsets = admin_auth_django.UserAdmin.fieldsets + (
+    #     ('Cargo', {'fields': ('cargo',)}),
+    # )
